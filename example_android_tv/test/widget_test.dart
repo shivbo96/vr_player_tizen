489635662260c720451a45cbdb78442afa3c1955
Shivam Mishra <shivbo.mishra96@gmail.com>
2026-02-27 13:16:20 +0530

    rename project from eample_1 to example_android_tv and update dependencies
    - Rename project directory and package name to `example_android_tv`.
    - Update project references in `pubspec.yaml` and `widget_test.dart`.
    - Update `pubspec.lock` with minor version bumps for `characters`, `matcher`, `material_color_utilities`, and `test_api`.
    - Relocate all Android and iOS configuration files to the new project structure.

--- example_android_tv/test/widget_test.dart
+++ example_android_tv/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:eample_1/main.dart';
+import 'package:example_android_tv/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
