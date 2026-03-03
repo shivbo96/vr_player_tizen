4731813ab342b35d16809c5cdcce523b43300cc1
Shivam Mishra <shivbo.mishra96@gmail.com>
2026-03-03 12:30:10 +0530

    fix: remove non-existent flutter/plugin_registrar_manager.h include
    PluginRegistrarManager is already available through flutter/plugin_registrar.h
    in the Tizen cpp_client_wrapper. The separate header does not exist in the
    Tizen SDK toolchain.

--- tizen/src/vr_player_plugin.cc
+++ tizen/src/vr_player_plugin.cc
@@ -2,7 +2,6 @@
 
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar.h>
-#include <flutter/plugin_registrar_manager.h>
 #include <flutter/standard_method_codec.h>
 
 #include <memory>
